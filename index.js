4bff276e1111b5d5bb2dc68bab29583e18fba450
homeswiz <homes@wizschool.io>
2021-12-31 22:01:40 +0900

    add user router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 const { PORT }  = require("./config");
+const userRouter = require("./routes/user");
+
+app.use("/user", userRouter);
 
 app.listen(PORT, function (){
   console.log(`app listening at PORT :${PORT}`);
