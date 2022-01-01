45035fab2b504e2b3e20218b1de28b7e942568b4
homeswiz <homes@wizschool.io>
2022-01-01 18:26:07 +0900

    Update login function

--- routes/user.js
+++ routes/user.js
@@ -12,4 +12,14 @@ router.post("/signUp", async (req, res, next) => {
     }
 });
 
+router.post("/login", async (req, res, next) => {
+    try {
+        const userDto = req.body;
+        const result = await new UserService().Login(userDto);
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+    }
+})
+
 module.exports = router;
