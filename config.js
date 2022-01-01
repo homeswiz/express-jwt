49a5a244bf1b27a77490e7e049333f320d572d7e
homeswiz <homes@wizschool.io>
2022-01-01 18:04:21 +0900

    Add config with dotenv

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  SERVER_PORT : process.env.PORT
+  SERVER_PORT : process.env.SERVER_PORT,
+  DB_HOST : process.env.DB_HOST,
+  DB_PORT : process.env.DB_PORT,
+  DB_DIALECT : process.env.DB_DIALECT,
+  DB_DATABASE : process.env.DB_DATABASE,
+  DB_USER : process.env.DB_USER,
+  DB_PASSWORD : process.env.DB_PASSWORD,
 }
