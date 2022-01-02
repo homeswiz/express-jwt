315a732b50a32bb49d30c7dec434fa4a17a96487
homeswiz <homes@wizschool.io>
2022-01-02 19:59:57 +0900

    Add default test GET /

--- test/test.js
+++ test/test.js
@@ -5,40 +5,11 @@ const { expect } = require('chai');
 describe("API Test", () => {
 
 	describe("GET /", () => {
-		it("should return healthy", () => {
+		it("should return healthy", (done) => {
 			request(server)
 				.get("/")
 				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					} else {
-						expect(res.body).equal("healthy");
-					}
-					return done();
-				  });
-		})
-	})
-
-	describe("POST /user/signUp", () => {
-		it("should return true", (done) => {
-
-			const userDto = { 
-				email: "test@gmail.com",
-				password: "123123",
-				name: "test"
-			}
-
-			request(server)
-				.post("/user/singUp", {
-					json: true
-				})
-				.send(userDto)
-				.expect(200)
-				.end(function(err, res) {
-					if (err) return done(err);
-					return done();
-				  });
+				.end(done);
 		})
 	})
 })
