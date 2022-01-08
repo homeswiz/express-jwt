33b4e9cddbf63f31c0acae229e39e50b7a1b8839
homeswiz <homes@wizschool.io>
2022-01-08 15:17:19 +0900

    update class Kakao property

--- util/oauth.js
+++ util/oauth.js
@@ -1,5 +1,21 @@
+const { 
+    KAKAO_ACCESS_TOKEN_GENERATE_URL,
+    KAKAO_USER_INFO_URL,
+    KAKAO_CLIENT_ID, 
+    KAKAO_CLIENT_SECRET ,
+    KAKAO_REDIRECT_URI
+} = require("../config");
+
 class Kakao {
-    constructor () {}
+    constructor (code) {
+        this.accessTokenGenerateUrl = KAKAO_ACCESS_TOKEN_GENERATE_URL;
+        this.userInfoUrl = KAKAO_USER_INFO_URL;
+        this.clientId = KAKAO_CLIENT_ID;
+        this.clientSecret = KAKAO_CLIENT_SECRET;
+        this.redirectUri = KAKAO_REDIRECT_URI;
+        this.code = code;
+        this.userInfoUrl = "";
+    }
 }
 
 class Naver {
