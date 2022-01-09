8ea37e502bc9d628e7bc937ef69951e7d42e2dcd
homeswiz <homes@wizschool.io>
2022-01-09 16:19:03 +0900

    update util oauth Naver

--- util/oauth.js
+++ util/oauth.js
@@ -3,7 +3,13 @@ const {
     KAKAO_USER_INFO_URL,
     KAKAO_CLIENT_ID, 
     KAKAO_CLIENT_SECRET ,
-    KAKAO_REDIRECT_URI
+    KAKAO_REDIRECT_URI,
+    NAVER_ACCESS_TOKEN_GENERATE_URL,
+    NAVER_USER_INFO_URL,
+    NAVER_AUTHORIZE_URL,
+    NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET,
+    NAVER_REDIRECT_URI,
 } = require("../config");
 
 class Kakao {
@@ -18,7 +24,15 @@ class Kakao {
 }
 
 class Naver {
-    constructor () {}
+    constructor (code) {
+        this.authorizationUrl = NAVER_AUTHORIZE_URL;
+        this.accessTokenGenerateUrl = NAVER_ACCESS_TOKEN_GENERATE_URL;
+        this.userInfoUrl = NAVER_USER_INFO_URL;
+        this.clientId = NAVER_CLIENT_ID;
+        this.clientSecret = NAVER_CLIENT_SECRET;
+        this.redirectUri = NAVER_REDIRECT_URI;
+        this.code = code;
+    }
 }
 
 class Google {
