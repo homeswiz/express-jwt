2c8f6a624099a3ee9bb57fa6af8c328933a5ec62
homeswiz <homes@wizschool.io>
2022-03-26 18:54:16 +0900

    Add: abstract class RouteConfig

---
+++ ts/config/routesConfig.ts
@@ -0,0 +1,18 @@
+import express from "express";
+export abstract class RouteConfig {
+
+    app: express.Application;
+    name: string
+
+    constructor(app: express.Application, name: string) {
+        this.app = app;
+        this.name = name;
+        this.configureRoute();
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    abstract configureRoute(): express.Application
+} 
